7e588b3bbaa80cfab60312683ba9a107e2929d63
RainbowJM <jevona98@live.com>
2022-11-02 09:13:55 +0000

    backend connection with the frontend

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {
+      src: '~/plugins/LiskClient.js',
+      ssr: false
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
