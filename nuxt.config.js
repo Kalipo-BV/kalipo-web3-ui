a4d61ad84d30fdf82be548c530982d4cfa6e32df
RainbowJM <jevona98@live.com>
2022-11-02 09:02:17 +0000

    head title updated and removed comments

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - web3-ui',
-    title: 'web3-ui',
+    // titleTemplate: '%s - web3-ui',
+    title: 'Kalipo',
     htmlAttrs: {
       lang: 'en'
     },
