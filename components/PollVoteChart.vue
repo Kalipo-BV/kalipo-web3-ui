9333bf16abb293be370f9789c90632814a9bca0b
RainbowJM <jevona98@live.com>
2022-11-23 22:19:48 +0000

    deleted some console.log, chart done

--- components/PollVoteChart.vue
+++ components/PollVoteChart.vue
@@ -1,28 +1,61 @@
 <template>
-    <v-card elevation="2" class="ma-3 mx-auto">
-        <v-card-title>
-            Results
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-            <div>
-                <apexchart width="340" type="donut" :options="chartOptions" :series="series"></apexchart>            
-            </div>
-        </v-card-text>
-    </v-card>
+  <v-card elevation="0" class="ma-3 mx-auto">
+    <v-card-title>
+      Results
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-text>
+      <div class="align-center justify-centerc">
+        <apexchart width="340" type="bar" :options="chartOptions" :series="series"></apexchart>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
   
 <script>
 export default {
-  props: ['countPerAnswer', 'answer'],
-  data: function() {
-    return {
-      series:[400, 430, 448, 470, 540],
-        chartOptions: {
-          chart: {
-            type: 'donut',
-            },
-        },
+  props: ['countPerAnswer', 'choices'],
+  data: () => ({
+    series: [{
+      data: []
+    }],
+    chartOptions: {
+      chart: {
+        type: 'bar',
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: true
+      },
+      xaxis: {
+        categories: [],
+      },
+      legend: {
+        show: false
+      }
+    },
+  }),
+  // data: () => ({
+  //   series: [],
+  //   chartOptions: {
+  //     chart: {
+  //       width: 340,
+  //       type: 'donut',
+  //     },
+  //     labels: []
+  //   },
+  // }),
+  async mounted() {
+    this.series.push({'data': this.countPerAnswer})
+
+    for (let i = 0; i < this.choices.length; i++) {
+      this.chartOptions.xaxis.categories.push(this.choices[i])
     }
   }
 }
--- pages/auton/_autonId/proposal/_proposalId/voting.vue
+++ pages/auton/_autonId/proposal/_proposalId/voting.vue
@@ -283,7 +283,6 @@ export default {
     const autonIdWrapper = await this.$invoke("auton:getAutonIdByName", {
       name: autonIdParam,
     });
-    console.log(autonIdWrapper);
     if (autonIdWrapper.result === null) {
       this.auton = null;
       this.error = "Auton not found: " + autonIdParam;
@@ -342,7 +341,6 @@ export default {
         this.statement = this.proposal.multiChoicePollArguments.question;
         for (let i = 0; i < this.proposal.multiChoicePollArguments.answers.length; i++) {
           this.answers.push(this.proposal.multiChoicePollArguments.answers[i].answer);
-          console.log(this.proposal.multiChoicePollArguments.answers[i].answer);
           this.countPerAnswer.push(parseInt(this.proposal.multiChoicePollArguments.answers[i].count));
           this.totalCounts += parseInt(this.proposal.multiChoicePollArguments.answers[i].count);
         }
@@ -353,7 +351,7 @@ export default {
               this.voted = true;
             }
           }
-      }
+        }
 
         if (this.proposal.type == "membership-invitation") {
           this.transaction.assetId = 0;
@@ -399,8 +397,6 @@ export default {
 
         const client = await this.$client();
         client.subscribe("vote:newVote", async (data) => {
-          console.log("NEW  VOTE");
-          console.log(data);
           if (
             !this.voteIds.includes(data.id) &&
             data.vote.proposalId == this.proposalId
@@ -417,14 +413,10 @@ export default {
         });
 
         client.subscribe("proposal:gotDecided", async (data) => {
-          console.log("GOT DECIDED");
-          console.log(data);
           if (data.id == this.proposalId) {
             this.proposal = data.proposal;
           }
         });
-        console.log(this.proposal);
-
       }
     }
   },
