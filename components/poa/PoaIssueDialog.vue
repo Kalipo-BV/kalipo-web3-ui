f4f13e3d48f437429470e7e05f49b5d7cfd7864d
Skiroe <r.s.canak@gmail.com>
2022-12-19 21:51:12 +0100

    checkin feature in progress

--- components/poa/PoaIssueDialog.vue
+++ components/poa/PoaIssueDialog.vue
@@ -1,4 +1,4 @@
-<template lang="">
+<template >
   <div>
     <v-card>
       <v-card-text v-if="step == 0">
---
+++ pages/dialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <check-in-dialog :auton="this.auton"></check-in-dialog>
+</template>
+
+<script>
+import CheckInDialog from "~/components/lesson/CheckInDialog";
+export default {
+  name: "dialog",
+  components: {CheckInDialog},
+  data: () => ({
+    auton: {
+      autonProfile: {
+        name: "AutonName",
+      },
+      lessonProfile: {
+        lessonName: "Default lesson moment",
+        subject: "pe3",
+        description: "Default lesson moment description",
+      },
+    },
+    methodes: {
+      // TODO: get the real autonId with UUID shizzel
+      findScannedAuton() {
+        const UUID = this.$route.params.uuid;
+
+
+        // this.auton = foundAuton;
+      },
+    },
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
