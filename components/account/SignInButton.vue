07ae40a7069c3e8473ebcfab549b3f1a34316551
hesteren <74653870+hesteren@users.noreply.github.com>
2023-01-16 11:10:00 +0100

    if (!element) {               continue;             }

--- components/account/SignInButton.vue
+++ components/account/SignInButton.vue
@@ -1,49 +1,45 @@
-<template >
-    <div class="pa-4">
-      <v-card
-        link
-        flat
-        color="primary lighten-1"
-        v-if="unlocked"
-        @click="$router.push('/account_settings')"
-      >
-        <v-card-text>
-          <div class="d-flex align-center justify-space-between">
-            <div class="d-flex align-center">
-              <v-avatar color="white" size="35"
+<template>
+  <div class="pa-4">
+    <v-card
+      link
+      flat
+      color="primary lighten-1"
+      v-if="unlocked"
+      @click="$router.push('/account_settings')"
+    >
+      <v-card-text>
+        <div class="d-flex align-center justify-space-between">
+          <div class="d-flex align-center">
+            <v-avatar color="white" size="35"
               ><div class="text-caption">
                 {{ getInitials(account.name) }}
               </div></v-avatar
-              >
-              <div class="white--text ml-2">
-                <div class="text-body-caption">{{ account.name }}</div>
-                <div class="text-caption">@{{ account.username }}</div>
-              </div>
-            </div>
-            <v-btn fab x-small color="white" @click="lockAccount"
-            ><v-icon color="primary lighten-1"
-            >mdi-lock-open</v-icon
-            ></v-btn
             >
+            <div class="white--text ml-2">
+              <div class="text-body-caption">{{ account.name }}</div>
+              <div class="text-caption">@{{ account.username }}</div>
+            </div>
           </div>
-        </v-card-text>
-      </v-card>
-      <v-card
-        link
-        flat
-        color="primary lighten-1"
-        v-if="!unlocked"
-        @click="$router.push('/wallet')"
-      >
-        <v-card-text>
-          <div
-            class="d-flex align-center justify-center text-h6 white--text"
+          <v-btn fab x-small color="white" @click="lockAccount"
+            ><v-icon color="primary lighten-1">mdi-lock-open</v-icon></v-btn
           >
-            Sign-in
-          </div>
-        </v-card-text>
-      </v-card>
-    </div>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card
+      link
+      flat
+      color="primary lighten-1"
+      v-if="!unlocked"
+      @click="$router.push('/wallet')"
+    >
+      <v-card-text>
+        <div class="d-flex align-center justify-center text-h6 white--text">
+          Sign-in
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -52,13 +48,12 @@ export default {
   props: {
     account: {
       type: Object,
-    }
+    },
   },
   computed: {
     unlocked() {
       return this.$store.state.wallet.unlocked;
     },
-
   },
   methods: {
     getInitials(parseStr) {
@@ -68,6 +63,9 @@ export default {
         for (let index = 0; index < nameList.length; index++) {
           if (index < 3) {
             const element = nameList[index];
+            if (!element) {
+              continue;
+            }
             result += element[0].toUpperCase();
           } else {
             break;
@@ -81,9 +79,7 @@ export default {
       this.$router.push("/wallet");
     },
   },
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
