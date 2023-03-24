255e65b68e161aa2353bb9479cc5c19bd635e4e2
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2023-03-24 15:03:53 +0100

    changed store architecture

---
+++ store/contract/validation.js
@@ -0,0 +1,33 @@
+export const isArray = (payload, errorName) => {
+	const result = (Array.isArray(payload));
+	logError(result, errorName);
+
+	return result;
+}
+
+export const isString = (payload, errorName) => {
+	const result = (typeof payload === 'string');
+	logError(result, errorName);
+
+	return result;
+}
+
+export const isNumber = (payload, errorName) => {
+	const result = (typeof Number.parseFloat(payload) === 'number');
+	logError(result, errorName);
+
+	return result;
+}
+
+export const isDate = (payload, errorName) => {
+	const result = (payload instanceof DATE);
+	logError(result, errorName);
+
+	return result;
+}
+
+export const logError = (bool, errorName) => {
+	if (!bool) {
+		console.error(`invalid ${errorName} given`, payload);
+	}
+}
