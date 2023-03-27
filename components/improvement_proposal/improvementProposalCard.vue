9fd163ac0c89b516397a56d21cfb62bb77d792ee
damianbrands <daim2300@gmail.com>
2023-03-27 13:59:48 +0200

    removed unnecessary divs

--- components/improvement_proposal/improvementProposalCard.vue
+++ components/improvement_proposal/improvementProposalCard.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="test" @click="toProposal(proposal.link)">
 
-<!--  min-width: 30%; min-height: 20vh; border-radius: 7px; padding: 10px 3% 10px 20px;-->
+    <!--  min-width: 30%; min-height: 20vh; border-radius: 7px; padding: 10px 3% 10px 20px;-->
     <div class="white"
          :style="{display: 'flex', margin: '0 10px 0 10px'}">
-      <div style="width: 100%; display: grid">
-        <div tabindex="0" class="v-card v-card--flat v-card--link v-sheet theme--light white"
-             :style="{ width: '300px', padding: '20px 30px 20px 30px', borderRadius: '7px', display: 'flex' }">
-          <div :style="{width: '100%'}">
-            <p style="color: #0F6AF1;" class="text-h1 mb-2">{{ proposal.title }}</p>
+      <div tabindex="0" class="v-card v-card--flat v-card--link v-sheet theme--light white"
+           :style="{ width: '300px', padding: '20px 30px 20px 30px', borderRadius: '7px', display: 'inline-block' }">
+        <div>
+          <p style="color: #0F6AF1;" class="text-h1 mb-2">{{ proposal.title }}</p>
 
-            <div>
-              <p class="mb-0">
-                Type: {{ proposal.type }}
-              </p>
-              <v-btn outlined small @click="$router.push('/account/' + proposal.author)"
-              >@{{ proposal.author }}
-              </v-btn>
-              <v-chip :color="getResultColor(proposal.result)" dark outlined small>
-                {{ proposal.result }}
-              </v-chip>
+          <div>
+            <p class="mb-0">
+              Type: {{ proposal.type }}
+            </p>
+            <v-btn outlined small @click="$router.push('/account/' + proposal.author)"
+            >@{{ proposal.author }}
+            </v-btn>
+            <v-chip :color="getResultColor(proposal.result)" dark outlined small>
+              {{ proposal.result }}
+            </v-chip>
 
-              <p>{{
+            <p>{{
                 new Date(parseInt(proposal.submission) * 1000).toLocaleDateString(
                   userLang,
                   {
@@ -33,14 +32,12 @@
                   }
                 )
               }}</p>
-
-            </div>
           </div>
-          <div class="d-flex align-center" :style="{width: '10%'}">
-            <p class="font-weight-regular" :style="{color: '#0F6AF1', marginBottom:'0px', fontsize: 'xx-large'}">
-              >
-            </p>
-          </div>
+        </div>
+        <div class="d-flex align-center" :style="{width: '10%'}">
+          <p class="font-weight-regular" :style="{color: '#0F6AF1', marginBottom:'0px', fontsize: 'xx-large'}">
+            >
+          </p>
         </div>
       </div>
     </div>
