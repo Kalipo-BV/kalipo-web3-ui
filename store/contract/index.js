4a5214e6f33bf795a8cee1912500940b081f44ae
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2023-03-29 16:20:25 +0200

    added reset functionality to the store

--- store/contract/index.js
+++ store/contract/index.js
@@ -17,30 +17,32 @@
 
 import { isArray, isObject, isBoolean, isDate, isNumber, isString, isValidPartyData } from "./validation.js"
 
-const initFormData = {
-	parties: {
-		contractor: [],
-		client: []
-	},
-	preample: null,
-	purpose: null,
-	payment: {
-		amount: null,
-		note: null,
-	},
-	dates: {
-		startDate: new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().substr(0, 10),
-		endDate: new Date(new Date().setDate(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).getDate() + 1)).toISOString().substr(0, 10),
-	},
-	propertyRights: null,
-	terminationOfAgreement: null,
-	governingLawAndJurisdiction: null,
-	finalProvisions: null,
-	milestones: [],
-	custom: [],
-	signingWindow: 0,
-	requiredToSign: false,
-	signed: false,
+const initFormData = () => {
+	return {
+		parties: {
+			contractor: [],
+			client: []
+		},
+		preample: null,
+		purpose: null,
+		payment: {
+			amount: null,
+			note: null,
+		},
+		dates: {
+			startDate: new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+			endDate: new Date(new Date().setDate(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).getDate() + 1)).toISOString().substr(0, 10),
+		},
+		propertyRights: null,
+		terminationOfAgreement: null,
+		governingLawAndJurisdiction: null,
+		finalProvisions: null,
+		milestones: [],
+		custom: [],
+		signingWindow: 0,
+		requiredToSign: false,
+		signed: false,
+	}
 }
 
 export const state = () => ({
@@ -49,7 +51,7 @@ export const state = () => ({
 	type: "Grant Contract",
 	fullySigned: false,
 	date: new Date().toISOString(),
-	formData: initFormData,
+	formData: initFormData()
 })
 
 export const mutations = {
@@ -169,6 +171,8 @@ export const mutations = {
 
 	saveContract() {
 		console.log(state);
+	reset(state) {
+		state.formData = initFormData();
 	}
 }
 
