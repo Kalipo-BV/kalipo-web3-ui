a30a315f5a84c75d73d19a3ed59940198f350436
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2023-04-12 14:23:24 +0200

    added localstorage functions

---
+++ store/contract/localstorage.js
@@ -0,0 +1,22 @@
+import { isValidContract } from "./validation.js"
+
+
+export const saveToLocalstorage = (contract) => {
+	const data = JSON.stringify(contract);
+	localStorage.setItem("Agreements", data);
+}
+
+export const getFromLocalstorage = () => {
+	if (localStorage.getItem("Agreements") == null) {
+		console.warning("no Agreements in localstorage");
+		return null;
+	}
+
+	const contract = JSON.parse(localStorage.getItem("Agreements"));
+
+	if (!isValidContract(contract)) {
+		console.error(`no valid contract in local storage\n\n ${contract}`);
+	}
+
+	return contract;
+}
