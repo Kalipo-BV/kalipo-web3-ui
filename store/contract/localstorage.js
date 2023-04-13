9da262a60a155269d67f42ecb23a5d3c35b90b21
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2023-04-13 17:14:26 +0200

    added fallback for agreement loading if its empty

--- store/contract/localstorage.js
+++ store/contract/localstorage.js
@@ -1,8 +1,10 @@
 import { isValidContract } from "./validation.js"
+import { initState } from "./initData.js";
 
 export const saveToLocalstorage = (contract) => {
 	const data = JSON.stringify(contract);
 	localStorage.setItem("Agreements", data);
+
 }
 
 export const getFromLocalstorage = () => {
@@ -14,8 +16,57 @@ export const getFromLocalstorage = () => {
 	const contract = JSON.parse(localStorage.getItem("Agreements"));
 
 	if (!isValidContract(contract)) {
-		console.error(`no valid contract in local storage\n\n ${contract}`);
+		const data = extractDataByObject(initState(), contract);
+		console.warn("getFromLocalstorage uses a fallback to fill the holes in its required dataStructure");
+
+		return data;
 	}
 
 	return contract;
+}
+
+function extractDataByObject(requiredObject, givenObject) {
+	const result = {};
+
+	for (const key in requiredObject) {
+		const currentRequiredProp = requiredObject[key];
+		const currentGivenProp    = givenObject[key];
+
+		if (currentGivenProp === undefined || currentGivenProp === null) {
+			result[key] = currentRequiredProp;
+			console.warn(`uses a fallback for the following key -> ${key}\n key is not defined or null`);
+			continue;
+		}
+
+
+		
+		const isRequiredPropObject = (typeof currentRequiredProp === 'object' && !Array.isArray(currentRequiredProp));
+		const isGivenPropObject = (typeof currentGivenProp === 'object' && !Array.isArray(currentGivenProp));
+		if (isRequiredPropObject) {
+			if (isGivenPropObject) {
+				const data = extractDataByObject(currentRequiredProp, currentGivenProp);
+				result[key] = data;
+
+			} else {
+				result[key] = currentRequiredProp
+				console.warn(`uses a fallback for the following key -> ${key}\n key is not an object`);
+			}
+			continue;
+		}
+
+
+
+		if (Array.isArray(currentRequiredProp)) {
+			if (Array.isArray(currentGivenProp)) {
+				result[key] = currentGivenProp;
+			} else {
+				result[key] = currentRequiredProp
+				console.warn(`uses a fallback for the following key -> ${key}\n key is not an array`);
+			}
+			continue;
+		}
+
+		result[key] = currentGivenProp;
+	}
+	return result;
 }
