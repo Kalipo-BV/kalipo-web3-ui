4ae04e608c568e9ca1649abd0385876ab7e24ba3
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2023-04-24 16:18:39 +0200

    Revert "small data fix by carebear"
    This reverts commit bd0e0066b70d05c5888a6c9e60e0c4676ce487e8.

--- store/contract/initData.js
+++ store/contract/initData.js
@@ -11,58 +11,58 @@ export const initState = () => {
 
 export const initFormData = () => {
 	return {
-		parties: {
-			contractor: [],
-			client: [],
-		},
-		preample: null,
-		purpose: null,
-		payment: {
-			amount: null,
-			note: null,
-		},
-		dates: {
-			startDate: new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().substr(0, 10),
-			endDate: new Date(new Date().setDate(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).getDate() + 1)).toISOString().substr(0, 10),
-		},
-		propertyRights: null,
-		terminationOfAgreement: null,
-		governingLawAndJurisdiction: null,
-		finalProvisions: null,
-		milestones: [],
-		custom: [],
-		signingWindow: null,
-		requiredToSign: false,
-		signed: false,
-
 		// parties: {
-		// 	contractor: [
-		// 		"f2131b1ec164f1e32f148378d9b61ba9fbddef1d9b6b212028ddd86f2b554db6",
-		// 		"b61d53937da7a3c9d81ea5a9cdcb88de3a742494d61d0c415fd50373c6dc8215"
-		// 	],
-		// 	client: [
-		// 		"f2131b1ec164f1e32f148378d9b61ba9fbddef1d9b6b212028ddd86f2b554db6",
-		// 		"b61d53937da7a3c9d81ea5a9cdcb88de3a742494d61d0c415fd50373c6dc8215"
-		// 	]
+		// 	contractor: [],
+		// 	client: []
 		// },
-		// preample: "123123",
-		// purpose: "231123123",
+		// preample: "",
+		// purpose: "",
 		// payment: {
-		// 	amount: 132,
-		// 	note: "123123"
+		// 	amount: "",
+		// 	note: "",
 		// },
 		// dates: {
-		// 	startDate: "2023-04-05",
-		// 	endDate: "2023-04-06"
+		// 	startDate: new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+		// 	endDate: new Date(new Date().setDate(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).getDate() + 1)).toISOString().substr(0, 10),
 		// },
-		// propertyRights: "123123",
-		// terminationOfAgreement: "asdadad",
-		// governingLawAndJurisdiction: "wdadawdawd",
-		// finalProvisions: "123123",
+		// propertyRights: "",
+		// terminationOfAgreement: "",
+		// governingLawAndJurisdiction: "",
+		// finalProvisions: "",
 		// milestones: [],
 		// custom: [],
-		// signingWindow: "",
+		// signingWindow: 0,
 		// requiredToSign: false,
-		// signed: false
+		// signed: false,
+
+		parties: {
+			contractor: [
+				"f2131b1ec164f1e32f148378d9b61ba9fbddef1d9b6b212028ddd86f2b554db6",
+				"b61d53937da7a3c9d81ea5a9cdcb88de3a742494d61d0c415fd50373c6dc8215"
+			],
+			client: [
+				"f2131b1ec164f1e32f148378d9b61ba9fbddef1d9b6b212028ddd86f2b554db6",
+				"b61d53937da7a3c9d81ea5a9cdcb88de3a742494d61d0c415fd50373c6dc8215"
+			]
+		},
+		preample: "123123",
+		purpose: "231123123",
+		payment: {
+			amount: 132,
+			note: "123123"
+		},
+		dates: {
+			startDate: "2023-04-05",
+			endDate: "2023-04-06"
+		},
+		propertyRights: "123123",
+		terminationOfAgreement: "asdadad",
+		governingLawAndJurisdiction: "wdadawdawd",
+		finalProvisions: "123123",
+		milestones: [],
+		custom: [],
+		signingWindow: "",
+		requiredToSign: false,
+		signed: false
 	}
 }
