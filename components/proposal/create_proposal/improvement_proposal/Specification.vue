62dce7d411a30ab4c39fd78c5f56d8be990f1a42
damianbrands <daim2300@gmail.com>
2023-04-24 11:15:07 +0200

    Specification made

--- components/proposal/create_proposal/improvement_proposal/Specification.vue
+++ components/proposal/create_proposal/improvement_proposal/Specification.vue
@@ -1,10 +1,33 @@
 <template>
-  <p>Specification input</p>
+  <div>
+    <v-form v-model="valid" @submit.prevent>
+      <v-textarea
+        filled
+        auto-grow
+        label="Specification"
+        rows="4"
+        row-height="20"
+        maxlength="1024"
+        counter
+        v-model="specificationValue"
+      ></v-textarea>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Specification"
+  props: ["specification"],
+  computed: {
+    specificationValue: {
+      get: function () {
+        return this.specification;
+      },
+      set: function (newValue) {
+        this.$emit("update:specification", newValue);
+      },
+    }
+  }
 }
 </script>
 
