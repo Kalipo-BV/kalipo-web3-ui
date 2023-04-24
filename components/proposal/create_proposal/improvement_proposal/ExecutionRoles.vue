f34d7b1f7c66a52f864d705914b6fc94dffde92a
damianbrands <daim2300@gmail.com>
2023-04-24 11:14:37 +0200

    Execution roles made

--- components/proposal/create_proposal/improvement_proposal/ExecutionRoles.vue
+++ components/proposal/create_proposal/improvement_proposal/ExecutionRoles.vue
@@ -1,10 +1,35 @@
 <template>
-  <p>Execution roles</p>
+  <div>
+    <v-form v-model="valid" @submit.prevent>
+      <li v-for="(role, index) in executionRoles">
+        <v-textarea
+          filled
+          auto-grow
+          label="Description"
+          rows="4"
+          row-height="0"
+          maxlength="32"
+          counter
+          v-model="executionRolesValue[index]"
+        ></v-textarea>
+      </li>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ExecutionRoles"
+  props: ["executionRoles"],
+  computed: {
+    executionRolesValue: {
+      get: function () {
+        return this.executionRoles;
+      },
+      set: function (newValue) {
+        this.$emit("update:executionRules", newValue);
+      },
+    }
+  }
 }
 </script>
 
