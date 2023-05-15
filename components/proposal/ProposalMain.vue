7647f33712adf9e0dbef0d595bb91274e0d6b7bf
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2023-05-15 12:11:47 +0200

    Update ProposalMain.vue

--- components/proposal/ProposalMain.vue
+++ components/proposal/ProposalMain.vue
@@ -217,12 +217,36 @@ export default {
         }),
       });
 
-      // Proposer
+      // Submitter
       this.list.push({
         icon: "mdi-account",
-        leftText: "Proposer:",
+        leftText: "Submitter:",
         rightText: "@" + this.submitter.username,
         link: "/account/" + this.submitter.username,
+      });
+
+      // proposers
+      var propserNames = "";
+      const promises = this.proposal.improvementArguments.proposers.map((proposerId) => {
+        return this.$invoke("kalipoAccount:getByID", {
+          id: proposerId,
+        });
+      });
+
+      await Promise.all(promises).then((accounts) => {
+        accounts.forEach((account) => {
+          console.log(account.result.name);
+          propserNames += account.result.name + ", "
+          console.log(propserNames)
+        });
+      });
+
+
+
+      this.list.push({
+        icon: "mdi-account-group",
+        leftText: "Authors:",
+        rightText:  propserNames,
       });
 
       // Proposal type
@@ -232,7 +256,7 @@ export default {
         rightText: this.proposal.type.replaceAll("-", " "),
       });
 
-      console.log(this.proposal)
+      
 
       // Some attributes linked to a proposal type.
       if (this.proposal.type == "membership-invitation") {
